99782a1277e174d00073710948d79523fef2ebee
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-03-07 19:54:19 +0330

    accessing from microservice to app -- testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
-
-import {MongooseModule} from '@nestjs/mongoose';
-import {UserSchema} from "./schema/user.schema";
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schema/user.schema';
 import { UsersController } from './users/users.controller';
 import { UserService } from './Services/users.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/TestDB"),
-    MongooseModule.forFeature([{
-      name:"Users",
-      schema:UserSchema,
-      collection: "Users"
-    }]),
+    // ClientsModule.register([
+    //   {
+    //     name: 'USERS',
+    //     transport: Transport.NATS,
+    //     options: {
+    //       url: 'nats://localhost:4222',
+    //     },
+    //   },
+    // ]),
+    MongooseModule.forRoot('mongodb://localhost:27017/TestDB'),
+    MongooseModule.forFeature([
+      {
+        name: 'Users',
+        schema: UserSchema,
+        collection: 'Users',
+      },
+    ]),
   ],
   controllers: [AppController, UsersController],
-  providers: [AppService,UserService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,52 @@
-import { Controller, Get, Post,Delete, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Logger,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { Client, Transport, ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { UserService } from '../Services/users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private service:UserService){}
-    @Get('all')
-    async GetAll(){
-        return await this.service.getAll();
-    }
+  constructor(private service: UserService) {}
+  logger = new Logger('AppController');
 
-    @Post('add')
-    async Add(@Body() user:any){
-        await this.service.add(user)
-    }
+  @Client({
+    transport: Transport.NATS,
+    options: {
+      url: 'nats://localhost:4222',
+    },
+  })
+  client: ClientProxy;
 
-    @Post('update')
-    async Update(@Body() user:any){
-        await this.service.update(user);
-    }
+  @Get('all')
+  async GetAll() {
+    return await this.service.getAll();
+  }
 
-    @Delete('delete/:id')
-    async Delete(@Param('id') id){
-        await this.service.delete(id);
-    }
+  @Post('add')
+  async Add(@Body() user: any) {
+    await this.service.add(user);
+  }
+
+  @Post('update')
+  async Update(@Body() user: any) {
+    await this.service.update(user);
+  }
+
+  @Delete('delete/:id')
+  async Delete(@Param('id') id) {
+    await this.service.delete(id);
+  }
+
+  @Get('users')
+  getTest(): Observable<any> {
+    this.logger.log('client#send -> topic: "testing" ');
+    return this.client.send('get-users', {});
+  }
 }
